3fc7e5718ffd18877e4e82f1956b4699cbe63ede
Storm <storm.sarracino01@gmail.com>
2021-06-18 14:57:16 -0500

    commit tree be9184342cf31f968c4b3c9a94cb87bdb5fe99ac

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = 'BOB';
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Ruben';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'not Ruben';
+}
